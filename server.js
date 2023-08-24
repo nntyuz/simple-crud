077dbf45b275c9820f4c330d7070936529fe8d5b
nnuyz <hellonedesing@gmail.com>
2023-08-24 21:23:35 +0300

    refactoring #1

--- server.js
+++ server.js
@@ -23,9 +23,6 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 let people = require("./data/people.json");
 
-app.listen(port, () => {
-  console.log(`Server listen on http://localhost:${port}/`);
-});
 app.use(bodyParser.json());
 
 const updateData = (response, value) => {
@@ -34,27 +31,24 @@ const updateData = (response, value) => {
   });
 };
 
-//Get All
-app.get("/api/people", (req, res) => {
+const getAll = (req, res) => {
   //когда мы отправляем через send мы отправляем text/html
   res.status(200).json({
     status: "success",
     count: people.length,
     people,
   });
-});
+};
 
-//Add
-app.post("/api/people", (req, res) => {
+const add = (req, res) => {
   const personId = people.length + 1;
   const person = req.body;
   people.push({ id: personId, ...person });
 
   updateData(res, `${person.name} has been added!`);
-});
+};
 
-//Get by id
-app.get("/api/people/:id", (req, res) => {
+const getById = (req, res) => {
   const paramId = Number(req.params.id);
   const person = people.find((a) => a.id === paramId);
 
@@ -64,10 +58,9 @@ app.get("/api/people/:id", (req, res) => {
   } else {
     res.status(404).send(`Person with ID: ${paramId} not founded!`);
   }
-});
+};
 
-//Update (patch)
-app.patch("/api/people/:id", (req, res) => {
+const update = (req, res) => {
   const paramId = Number(req.params.id);
   const personToUpdate = people.find((a) => a.id === paramId);
   const indexOfPerson = people[paramId - 1];
@@ -79,25 +72,8 @@ app.patch("/api/people/:id", (req, res) => {
   } else {
     res.status(404).send(`Person with ID: ${paramId} not founded!`);
   }
-});
-
-//Update (put)
-app.put("/api/people/:id", (req, res) => {
-  const paramId = Number(req.params.id);
-  const personToUpdate = people.find((a) => a.id === paramId);
-  const indexOfPerson = people[paramId - 1];
-
-  if (personToUpdate) {
-    Object.assign(personToUpdate, req.body);
-    people[indexOfPerson] = personToUpdate;
-    updateData(res, personToUpdate);
-  } else {
-    res.status(404).send(`Person with ID: ${paramId} not founded!`);
-  }
-});
-
-//Delete
-app.delete("/api/people/:id", (req, res) => {
+};
+const remove = (req, res) => {
   const paramId = Number(req.params.id);
   const personToDelete = people.find((a) => a.id === paramId);
 
@@ -107,4 +83,15 @@ app.delete("/api/people/:id", (req, res) => {
   } else {
     res.status(404).send(`Person with ID ${paramId} not founded!`);
   }
+};
+
+app.get("/api/people", getAll);
+app.post("/api/people", add);
+app.get("/api/people/:id", getById);
+app.patch("/api/people/:id", update);
+app.put("/api/people/:id", update);
+app.delete("/api/people/:id", remove);
+
+app.listen(port, () => {
+  console.log(`Server listen on http://localhost:${port}/`);
 });
